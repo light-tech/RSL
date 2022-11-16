5bea82611d010b261ec7f269780c1082ebe66b17
Chris Thrasher <chrisjthrasher@gmail.com>
2022-11-16 11:57:11 -0700

    Make `rsl::rng()` tests more strenuous
    Seeding on the first call tests a code path that hadn't yet been
    tested but is a valid use of the API.

--- tests/random.cpp
+++ tests/random.cpp
@@ -2,25 +2,27 @@
 
 #include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
 
 #include <thread>
 
 TEST_CASE("rsl::rng") {
-    auto* const rng = &rsl::rng();
-
     SECTION("Repeated calls in thread yield same object") {
+        auto* const rng = &rsl::rng({0, 1});
         CHECK(rng == &rsl::rng());
         CHECK(rng == &rsl::rng());
         CHECK(rng == &rsl::rng());
     }
 
     SECTION("Calls from separate threads yield separate objects") {
-        std::thread([rng]() { CHECK(rng != &rsl::rng()); }).join();
+        auto* const rng = &rsl::rng();
+        std::thread([rng]() { CHECK(rng != &rsl::rng({2, 3})); }).join();
     }
 
     SECTION("Throw if seeded after first call") {
         CHECK_NOTHROW(rsl::rng({}));
-        CHECK_THROWS(rsl::rng({1, 2, 3, 4}));
+        CHECK_THROWS_WITH(rsl::rng({1, 2, 3, 4}), Catch::Matchers::ContainsSubstring(
+                                                      "rng cannot be re-seeded on this thread"));
     }
 }
 
